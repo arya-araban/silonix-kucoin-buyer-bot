7cb44097d636f45690110313ac5e29b41a0b5558
arya-araban <arya.araban21@gmail.com>
2021-09-30 21:25:05 +0330

    added shell for selling on target percentage

--- pump.py
+++ pump.py
@@ -1,5 +1,6 @@
 import sys
-
+import time
+import threading
 from kucoin.client import Client
 from telethon import TelegramClient, events
 import config
@@ -10,9 +11,18 @@ tel_client = TelegramClient('anon', config.api_id, config.api_hash)
 # KuCoin API values
 kc_client = Client(config.api_key, config.api_secret, config.api_passphrase)
 
-# TUNABLE PARAMETERS
-CHANNEL_NAME = 'pmptst'  # kucoin_pumps OR KucoinPumpChannel
+# TUNABLE PARAMETERS ESSENTIALS!
+CHANNEL_NAME = 'pmptst'  # kucoin_pumps OR KucoinPumpChannel OR PumpItUp
 COIN_AMOUNT = '10000'  # coin amount to buy. Set this a high value to buy all of your current USDT
+
+
+def sell_on_target(sell_target_percentage=100, time_to_check=0.3):
+    """
+    sell_target_percentage is the target percentage we want to sell on as soon as we hit it. IE: 100% means we want 2x
+    profit time_to_check is in second.  it will check for target each n seconds. a good default value is 0.2
+    """
+    threading.Timer(time_to_check, sell_on_target).start()
+    print("Hello, World!")
 
 
 def extract_coin_name(txt):
@@ -22,7 +32,7 @@ def extract_coin_name(txt):
     return coin_name
 
 
-def main():
+def main(sell_target=False):
     @tel_client.on(events.NewMessage(chats=CHANNEL_NAME))
     async def my_event_handler(event):
         print(event.raw_text)
@@ -31,9 +41,12 @@ def main():
         order = kc_client.create_market_order(coin_name, Client.SIDE_BUY, size=COIN_AMOUNT)
         print(order)
 
+    if sell_target:
+        sell_on_target()
+
     tel_client.start()
     tel_client.run_until_disconnected()
 
 
 if __name__ == "__main__":
-    main()
+    main(sell_target=True)
