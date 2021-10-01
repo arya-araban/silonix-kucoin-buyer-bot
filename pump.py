de3d1a566a5cd4f589e1f049be3b1f45d991c30b
arya-araban <arya.araban21@gmail.com>
2021-10-02 01:01:01 +0330

    fully functional sell on target!

--- pump.py
+++ pump.py
@@ -5,9 +5,12 @@ from telethon import events
 from rsrcs.useful_funcs import extract_coin_name
 from rsrcs.coin_lib import sell_on_target
 
-# TUNABLE PARAMETERS!
+# ESSENTIAL TUNABLE PARAMETERS!
 CHANNEL_NAME = 'pmptst'  # kucoin_pumps OR KucoinPumpChannel OR PumpItUp
-COIN_AMOUNT = '10000'  # coin amount to buy. Set this a high value to buy all of your current USDT
+COIN_AMOUNT = '100000'  # coin amount to buy. Set this a high value to buy all of your current USDT
+
+# NON-ESSENTIAL
+SELL_TARGET_PERCENTAGE = 200
 
 
 def main(sell_target=False):
@@ -17,14 +20,19 @@ def main(sell_target=False):
 
         c_name = extract_coin_name(event.raw_text)
         order = kc_client.create_market_order(c_name + '-USDT', Client.SIDE_BUY, size=COIN_AMOUNT)
-        print(order)
+        entry_price = kc_client.get_fiat_prices(symbol=c_name)[c_name]  # or take from bid?
+        print(f"{order} --- {entry_price}")
 
         if sell_target:
-            sell_on_target(coin_name=c_name, entry_price=..., sell_target_percentage=2000, time_to_check=0.3)
+            num_decimals = kc_client.get_order_book(c_name + '-USDT')['bids'][0][0][::-1].find('.')
+            deal_amount = f'%.{num_decimals}f' % (float(kc_client.get_order(order['orderId'])['dealSize']))
+            target_price = f'%.{num_decimals}f' % (float(entry_price) * ((SELL_TARGET_PERCENTAGE / 100) + 1))
+            # the '%.2f' % is to limit decimals!
+            sell_on_target(coin_name=c_name, target_price=target_price, coin_amount=deal_amount, time_to_check=1)
 
     tel_client.start()
     tel_client.run_until_disconnected()
 
 
 if __name__ == "__main__":
-    main()
+    main(sell_target=True)
--- rsrcs/coin_lib.py
+++ rsrcs/coin_lib.py
@@ -1,24 +1,24 @@
 import threading
 
-from requests import Timeout
-
-from pump import kc_client, COIN_AMOUNT
+from config import kc_client
 from kucoin.client import Client
 
 
-def sell_on_target(coin_name, entry_price, sell_target_percentage, time_to_check=0.3):
+def sell_on_target(coin_name, target_price, coin_amount, time_to_check=0.3):
     """
     coin name example: 'BTC'
     entry price is the price of the succeeded order
     sell_target_percentage is the target percentage we want to sell on as soon as we hit it. IE: 100% means we want 2x
     profit time_to_check is in second.  it will check for target each n seconds. a good default value is 0.3
     """
-    my_timer = threading.Timer(time_to_check, sell_on_target)
-    my_timer.start()
-    target_price = entry_price * ((sell_target_percentage / 100) + 1)
 
-    if target_price < kc_client.get_fiat_prices(symbol=coin_name):
+    my_timer = threading.Timer(time_to_check, sell_on_target,
+                               args=[coin_name, target_price, coin_amount, time_to_check])
+    my_timer.start()
+
+    cur_price = kc_client.get_order_book(coin_name + '-USDT')['bids'][0][0]
+    if target_price < cur_price:
         order = kc_client.create_limit_order(coin_name + "-USDT", Client.SIDE_SELL, price=target_price,
-                                             size=COIN_AMOUNT)
-        print(order)
+                                             size=coin_amount)
+        print(f"{order} happened! selling on target price {str(target_price)}")
         my_timer.cancel()
