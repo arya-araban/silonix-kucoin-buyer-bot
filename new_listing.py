aa6c7278e8896d711470042c7179043766da6e03
arya-araban <arya.araban21@gmail.com>
2022-04-05 16:31:38 +0430

    added mechanism to automatically detect newly listed token + added keyboard sell to new listing

--- new_listing.py
+++ new_listing.py
@@ -1,24 +1,44 @@
 import time
-from rsrcs.coin_lib_listings import keyboard_buy, buy_on_time
 
-# TUNABLE PARAMETERS
-DESIRED_TIME_UTC = "11:00:02"  # Put this the time in which the coin will be releasing
-COIN_NAME = "ILA"
+from config import kc_client
+from rsrcs.coin_lib_listings import limit_buy_token, keyboard_buy, keyboard_sell_new_listing
+from rsrcs.coin_lib_pumps import keyboard_sell
 
-USDT_AMOUNT = 40  # amount of usdt to buy with. make sure you have enough USDT in your balance!
-RELEASE_OFFSET_PERCENTAGE = 3  # offset percentage to buy higher ie 1 means 1% higher. 0 means no price offset
-KEYBOARD_OFFSET_PERCENTAGE = 3
+COIN_NAME = 'DOGE'
+DELAY = 0  # delay to update coin price again as soon as listing. 0 means no update
+USDT_AMOUNT = 0.3
+OFFSET = 0  # percentage added to OG price. ie: if price 100 retrieved and offset is 1, then order price will be 101
 
 
-# around 10 to 50 percent for world premiers, and 1 to 5 for normal new listings.
+# good numbers for offset: around 5 to 30 percent for world premiers, and 1 to 5 for normal new listings.
 
 
 def main():
-    buy_on_time(COIN_NAME, USDT=USDT_AMOUNT, offset=RELEASE_OFFSET_PERCENTAGE,
-                desired_time_utc=DESIRED_TIME_UTC)
+    while True:  # keep getting coin until it has been listed!
+        coin = kc_client.get_fiat_prices(symbol=COIN_NAME)
+        if not coin:  # if coin hasn't been listed yet, go on next iteration
+            continue
+        break
 
-    keyboard_buy(COIN_NAME, USDT=USDT_AMOUNT, offset=KEYBOARD_OFFSET_PERCENTAGE)
+    if DELAY != 0:
+        time.sleep(DELAY)
+        coin = kc_client.get_fiat_prices(symbol=COIN_NAME)  # update coin info
+
+    cur_price = float(coin[COIN_NAME])
+
+    order_id = limit_buy_token(COIN_NAME, USDT_AMOUNT, OFFSET, cur_price)
+
+    keyboard_sell_new_listing(COIN_NAME, order_id)  # page-up -- limit sell, page-down -- market sell
+
+    '''
+    Uncomment the following line to enable keyboard buying, this will only be used for cases where limit order hasn't 
+    been successfully executed. 
+    
+    MAKE SURE TO CANCEL CURRENT ORDER WITH 'c', then you will be able to place a new limit order with 'b'
+    '''
+
+    # keyboard_buy(COIN_NAME, USDT_AMOUNT, OFFSET, order_id)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- rsrcs/coin_lib_listings.py
+++ rsrcs/coin_lib_listings.py
@@ -4,6 +4,8 @@ from config import kc_client
 from kucoin.client import Client
 from pynput.keyboard import Listener, KeyCode
 
+from rsrcs.coin_lib_pumps import keyboard_sell
+
 
 def buy_on_time(coin_name, USDT, offset, desired_time_utc):
     """ This function buys new listing on specified time - USED FOR NEW LISTINGS
@@ -20,20 +22,20 @@ def buy_on_time(coin_name, USDT, offset, desired_time_utc):
     print(now_gmt)
     if now_gmt == desired_time_utc:
         print('\n time buying new listing!')
-        __limit_buy_token(coin_name, USDT, offset)
+        limit_buy_token(coin_name, USDT, offset)
         my_timer.cancel()
-        # the order has happened on time, now stop this thread. we will enable
+        # the order has happened on time, now stop this thread.
 
 
-def keyboard_buy(coin_name, USDT, offset):
+def keyboard_buy(coin_name, USDT, offset, cur_order_id=0):
     """ This function is similar to buy_on_time, except that it buys with keyboard presses - USED FOR NEW LISTINGS
     press 'B' to create LIMIT ORDER on fiat price.
     press 'm' to buy market price! note: may not work due to new listing constraint. """
 
     def buy_keypress(*key):
-        cur_order_id = 0
+        nonlocal cur_order_id
         if key[0] == KeyCode.from_char('b'):
-            cur_order_id = __limit_buy_token(coin_name, USDT, offset)
+            cur_order_id = limit_buy_token(coin_name, USDT, offset)
 
         if key[0] == KeyCode.from_char('c'):
             kc_client.cancel_order(cur_order_id)
@@ -48,11 +50,13 @@ def keyboard_buy(coin_name, USDT, offset):
     threading.Thread(target=key).start()
 
 
-def __limit_buy_token(coin_name, USDT, offset):
+def limit_buy_token(coin_name, USDT, offset, cur_price=0):
     """ sets a limit order based on the token name, USDT amount, and offset to the amount. - USED FOR NEW LISTINGS
-    This functions is private, and used in the above functions.
+    This functions is used in the above functions.
      """
-    cur_price = float(kc_client.get_fiat_prices(symbol=coin_name)[coin_name])  # get fiat or use asks
+    if not cur_price:
+        cur_price = float(kc_client.get_fiat_prices(symbol=coin_name)[coin_name])  # get fiat or use asks
+
     cur_price += cur_price * (offset / 100)
 
     ord_bk_fa = kc_client.get_order_book(coin_name + '-USDT')['bids'][
@@ -64,5 +68,21 @@ def __limit_buy_token(coin_name, USDT, offset):
             USDT / cur_price)
     order_id = kc_client.create_limit_order(coin_name + "-USDT", Client.SIDE_BUY, price=cur_price,
                                             size=buy_amount)
-    print(f"limit buy order {order_id} happened!")
-    return order_id
+    print(f"limit buy order {order_id} placed!")
+    return order_id['orderId']
+
+
+def keyboard_sell_new_listing(coin_name, order_id):
+    """
+        This function sells with keyboard presses -  USED FOR PUMPS!
+        press 'pg up' to sell on market
+        press 'pg down' to sell on limit (which will be the highest buy ask for the coin)
+        usually the optimal time to sell is around one minute after new listing
+    """
+    ord_bk_fa = kc_client.get_order_book(coin_name + '-USDT')['bids'][0]  # order book first order
+    num_decimals_amount = ord_bk_fa[1][::-1].find('.')
+
+    deal_amount = f'%.{num_decimals_amount}f' % (float(kc_client.get_order(order_id)['dealSize']) * 0.998)
+    keyboard_sell(coin_name=coin_name,
+                  coin_amount=deal_amount,
+                  pairing_type='USDT')
